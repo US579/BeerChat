055ff932dd85d50c5ea0e5b19868fdb37c80df70
us579 <wzus579@gmail.com>
2019-11-07 20:16:02 +1100

    add huaci flag

--- frontend/background.js
+++ frontend/background.js
@@ -32,7 +32,7 @@ chrome.runtime.onMessage.addListener(
         "http://127.0.0.1:5000/ChatService/chatbot?message=" +
         request.contentScriptQuery +
         "?huaci=" +
-        request.huaci; ;
+        request.huaci;
         console.log(url)
         fetch(url, {
             method: "GET",
