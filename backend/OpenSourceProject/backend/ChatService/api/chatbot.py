fb72e0728e81a52df72307f61a3b237c0c987fa3
G <gk2033724664@gmail.com>
2019-11-21 22:46:50 +1100

    add comment

--- backend/OpenSourceProject/backend/ChatService/api/chatbot.py
+++ backend/OpenSourceProject/backend/ChatService/api/chatbot.py
@@ -9,6 +9,8 @@ import os
 import json
 from googletrans import Translator
 from wit import Wit
+
+# the access token for wit ai
 access_token = "ABJWGG53QBEVM6UY6AUMBPNP42EQCXOZ"
 client = Wit(access_token)
 rs = RiveScript()
@@ -16,57 +18,63 @@ rs1 = RiveScript()
 import datetime
 class Chatbot(Resource):
     def wit_response(self,text):
-        #print(json.dumps(text, indent=4))
-        #print(text["_text"])
         message = "Analysing the code " + text["_text"] + " : "
         entities = list(text["entities"].keys())
-        print(entities)
+        # print(entities)
+        # if the intent cannot be detected, redirect to rs
         if "intent" not in entities:
             return None
+
+        # using different ways to avoid mistranslate
         F = text['_text'].split()
-        if F[0]!="for" or "if"!=F[0] or "while"!=F[0]:
+        if F[0] not in ["for","if","while"]:
             return None
+        # in + range
         if "in" in F:
             index = F.index("in")
-            if index == len(F)-1 or F[index+1]!="range":
+            if index == len(F)-1 or len(F[index+1])<5 or F[index+1][:5]!="range":
                 return None
-        #print(text['_text'])
+        # while
+        if F[0] in ["for","if","while"] and len(F)>1:
+            FF = text['_text'].split(' ',1)
+            k = ["==", ">=", "<=",">","<", "!=","in"]
+            flag = 0
+            print(1)
+            for i in k:
+                if i in FF[1]:
+                    flag = 1
+            if flag!=1:
+                return None
+
+        # try to catch intent and response message
         try:
             intent = text["entities"]["intent"][0]["value"]
-            print(intent)
+            # print(intent)
             if intent == "function_search":
                 return None
             if intent == "Loop_function":
                 m_function ="This is a Loop Function. The key word: \" " +\
                     text["entities"]["loop_tag"][0]["value"] + "\" represents the loop."
             if intent == "Conditional_statement":
-                print("????:")
-                print(json.dumps(text["entities"],indent=4))
+                # print(json.dumps(text["entities"],indent=4))
                 if "loop_tag" in text["entities"]:
                     m_function = "This is a Loop Statement. The key word: \"" + \
                                  text["entities"]["loop_tag"][0]["value"] + "\" represents the loop starts."
                 else:
                     m_function = "This is a Condition Statement. The key word: \"" + \
                             text["entities"]["condition_statement_tag"][0]["value"] + "\" represents the condition starts."
-                print(m_function)
+                # print(m_function)
             if "condition" in entities:
-                # print("!!!")
-                # print(list(text["entities"]["condition"]))
-                # print(list(text["entities"]["condition"][0]))
-                # print(text["entities"]["condition"][0]["value"])
                 if "entities" not in list(text["entities"]["condition"][0]):
-                    #print("aaaa")
                     m_condition = ". The following is condition: \"" + text["entities"]["condition"][0]["value"]+ "\" ."
                 else:
                     condition_entities = list(text["entities"]["condition"][0]["entities"].keys())
-                    #print(condition_entities)
                     c = text["entities"]["condition"][0]["entities"]
 
                     m_condition = ". The following is condition: \"" +text["entities"]["condition"][0]["value"] + "\", it can split into several parts: "
                     for i in condition_entities:
                         if i!= "intent":
                             m_condition += "\"" + i  + "\" is " + c[i][0]["value"] +"; "
-                # print(m_condition)
             return message + m_function + m_condition
         except Exception:
             return None
